6c2d5381997e58a3a7efb72b9ea52be98e346ee1
Ashutosh Srivastava <erashu@live.in>
2021-07-04 01:51:43 +0530

    initial

---
+++ AirLineWeb/Data/AirlineDbContext.cs
@@ -0,0 +1,16 @@
+using AirLineWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace AirLineWeb.Data
+{
+    public class AirlineDbContext : DbContext
+    {
+        public AirlineDbContext(DbContextOptions<AirlineDbContext> opt) :base(opt)
+        {
+            
+        }        
+    
+        public DbSet<WebhookSubscription> WebhookSubscriptions { get; set; }
+    }
+}
